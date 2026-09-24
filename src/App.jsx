764c0827014432283586faa68e55c4508919ab12
Bruno <brunocarreirodossantos12@gmail.com>
2026-09-24 09:47:21 -0300

    feat: Codificando estilizando o header

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
-import {BrowserRouter as Router,Routes,Route }from 'react-router-dom'
-import {Header} from './components/Header'
-import {Footer} from './components/Footer'
-import {GameCard} from './components/GameCard'
-import {Home} from './pages/Home'
-import {Jogos} from './pages/Jogos'
-import {Login} from './pages/Login'
-import {Error} from './pages/Error'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import GameCard from './components/GameCard'
+import Home from './pages/Home'
+import Contato from './pages/Contato'
+import Jogos from './pages/Jogos'
+import Login from './pages/Login'
+import Error from './pages/Error'
 
 const App = () => {
   return (
     <Router>
       <div className='min-h-screen flex flex-col justify-between bg-black text-white' pt-4>
-        <Header>
-          <Routes>
-            <Route path='/' element={<Home/>}></Route>
-            <Route path='/jogos' element={<Jogos/>}></Route>
-            <Route path='/login' element={<Login/>}></Route>
-            <Route path='*' element={<Error/>}></Route>
-          </Routes>
-          <Footer></Footer>
-        </Header>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/contato' element={<Contato />}></Route>
+          <Route path='/jogos' element={<Jogos />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='*' element={<Error />}></Route>
+        </Routes>
+        <Footer />
       </div>
-      
+
     </Router>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+    <header className='flex justify-between items-center py-6 px-[5%] bg-[#0f172a] text-white'>
+      <h1 className='logo p-2 text-[2rem] font-bold text-white cursor-pointer transition-all'>
+        Loja <span className='text-blue-500 p-1'>Gamer</span>
+      </h1>
+      <nav>
+        <ul className="flex list-none items-center gap-8">
+          <li>
+            <Link to='/' className='text-white text-lg no-underline hover:text-blue-500 hover:uppercase transition-all'>Home</Link>
+          </li>
+          <li>
+            <Link to='/contato' className='text-white text-lg no-underline hover:text-blue-500 hover:uppercase transition-all'>Contato</Link>
+          </li>
+          <li>
+            <Link to='/login' className='text-white text-lg no-underline hover:text-blue-500 hover:uppercase transition-all'>Login</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
